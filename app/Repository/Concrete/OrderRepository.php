c1673acff144b8edba795d39906aac20424f4280
Elisha OYAWALE <temmyscope@Elishas-MacBook-Air.local>
2023-02-08 13:36:14 +0100

    add Order Repository Handlers and Configure its Binding

---
+++ app/Repository/Concrete/OrderRepository.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Repository\Concrete;
+
+use App\Repository\Interface\OrderRepositoryInterface;
+
+class OrderRepository implements OrderRepositoryInterface
+{
+
+  public function create(){
+    
+  }
+
+}
---
+++ app/Repository/Interface/OrderRepositoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Repository\Interface;
+
+interface OrderRepositoryInterface{
+
+  public function create(); #would require Request Handler
+  
+}
