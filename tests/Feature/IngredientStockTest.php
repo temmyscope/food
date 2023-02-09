ab5c3bc065442bd0a53b6ebe7345583ecaeb61a2
Elisha OYAWALE <temmyscope@Elishas-MacBook-Air.local>
2023-02-09 09:42:50 +0100

    add test scenarios and methods for order and ingredients stock

--- tests/Feature/IngredientStockTest.php
+++ tests/Feature/IngredientStockTest.php
@@ -2,12 +2,10 @@
 
 namespace Tests\Feature;
 
-use App\Models\Ingredient;
-use App\Models\Order;
-use App\Models\Product;
 use Tests\TestCase;
+use App\Models\Product;
+use App\Models\Ingredient;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class IngredientStockTest extends TestCase
 {
@@ -20,6 +18,49 @@ class IngredientStockTest extends TestCase
     **/
     public function test_stock_updates_correctly()
     {
+        /**
+         * Pre-order tests for stock update
+        **/
+        //product table should contain `Beef` before test runs
+        $this->assertDatabaseHas(Product::class, [
+            "name" => "Burger"
+        ]);
+
+        //ingredient table should contain appropriate ingredient and quantity(s)
+        $this->assertDatabaseHas(Ingredient::class, [
+            "name" => "Beef", "initial_qty" => 20000, "available_qty" => 20000
+        ]);
+        $this->assertDatabaseHas(Ingredient::class, [
+            "name" => "Cheese", "initial_qty" => 5000, "available_qty" => 5000
+        ]);
+        $this->assertDatabaseHas(Ingredient::class, [
+            "name" => "Onion", "initial_qty" => 1000, "available_qty" => 1000
+        ]);
+
+        //Order Operation
+        $response = $this->postJson('/api/order', [
+            "products" => [
+                [ "product_id" => 1, "quantity" => 2 ]
+            ]
+        ]);
+
+        /**
+         * Post-order tests for stock update
+        **/
+        //since 1 burger uses 150g of Beef, then 2 would use up (150*2)g
+        $this->assertDatabaseHas(Ingredient::class, [
+            "name" => "Beef", "initial_qty" => 20000, "available_qty" => (20000-300)
+        ]);
+        //since 1 burger uses 30g of Cheese,, then 2 would use up (30*2)g 
+        $this->assertDatabaseHas(Ingredient::class, [
+            "name" => "Cheese", "initial_qty" => 5000, "available_qty" => (5000-60)
+        ]);
+        //since 1 burger uses 20g of Onion, then 2 would use up (20*2)g
+        $this->assertDatabaseHas(Ingredient::class, [
+            "name" => "Onion", "initial_qty" => 1000, "available_qty" => (1000-40)
+        ]);
+    
+        $response->assertStatus(200);
     }
 
 }
--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -5,19 +5,66 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class OrderTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function test_order_is_valid()
+    {
+        //this order should fail because its structure would fail validation
+        $response = $this->postJson('/api/order', [
+            "products" => []
+        ]);
+        $response->assertStatus(422);
+
+        //this order should fail because it orders above the available ingredient stock
+        $response = $this->postJson('/api/order', [
+            "products" => [
+                [ "product_id" => 1, "quantity" => 3000 ]
+            ]
+        ]);
+        $response->assertStatus(422);
+
+
+        //this order should be successful because it's valid in structure and product quantity
+        $response = $this->postJson('/api/order', [
+            "products" => [
+                [ "product_id" => 1, "quantity" => 1 ]
+            ]
+        ]);
+        $response->assertStatus(201);
+    }
+
     /**
-     * test if the order persists in db
+     * test if the order succeeds and persists in db
      *
      * @return void
      */
-    public function test_order_persistence()
+    public function test_order_success_and_persistence()
     {
+        //order should be empty before first order is sent
+        $this->assertDatabaseEmpty(Order::class);
+
+        $response = $this->postJson('/api/order', [
+            "products" => [
+                [ "product_id" => 1, "quantity" => 1 ]
+            ]
+        ]);
+        
+        //order should contain 1 item after first order is sent
+        $this->assertDatabaseCount(Order::class, 1);
+
+        $response = $this->postJson('/api/order', [
+            "products" => [
+                [ "product_id" => 1, "quantity" => 2 ]
+            ]
+        ]);
+        
+        //order should contain 2 items after second order is sent
+        $this->assertDatabaseCount(Order::class, 2);
+    
+        $response->assertStatus(201);
     }
 
 }
