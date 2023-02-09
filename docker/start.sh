0b5c6ff2ba3564ec13886a98b861b41fc0c24f7b
Elisha OYAWALE <temmyscope@Elishas-MacBook-Air.local>
2023-02-09 10:44:13 +0100

    Add Redis Queue Configuration For Dispatching background Jobs

--- docker/start.sh
+++ docker/start.sh
@@ -10,6 +10,11 @@ if [ "$role" = "app" ]; then
   echo "Running the app..."
   exec php artisan octane:start --workers=auto --task-workers=auto --server=swoole --host=0.0.0.0 --port=8080
 
+elif [ "$role" = "queue" ]; then
+
+  echo "Running the queue..."
+  php artisan queue:work --verbose --tries=3 --timeout=90
+
 else
   echo "Could not match the container role \"$role\""
   exit 1
